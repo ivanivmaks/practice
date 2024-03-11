1f0a2e18f9dd3f2e3dd2363ccffc2ec53d988859
ivaniv_maks <ivanivmaks0@gmail.com>
2024-03-11 20:39:48 +0200

    c

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,41 +1,15 @@
-import { galleryItems } from "./gallery-items.js";
-const gallery = document.querySelector(".gallery");
+const user = {
+  name: "Mango", 
+  age: 2,
+};
+console.log(JSON.stringify(user));
+const data = '{ "name": "Mango", "age": 2 }';
+console.log(JSON.parse(data));
 
-galleryItems.forEach(({ preview, original, description }) => {
-  const galleryItem = document.createElement("li");
-  galleryItem.classList.add("gallery__item");
+localStorage.setItem("my-data", JSON.stringify(user));
 
-  const link = document.createElement("a");
-  link.classList.add("gallery__link");
-  link.href = original;
-  link.addEventListener("click", openImg);
+const savedData = localStorage.getItem("my-data");
+console.log('savedData:', savedData);
 
-  const img = document.createElement("img");
-  img.classList.add("gallery__image");
-  img.src = preview;
-  img.dataset.source = original;
-  img.alt = description;
-
-  galleryItem.appendChild(link);
-  link.appendChild(img);
-
-  gallery.append(galleryItem);
-});
-
-function openImg(event) {
-  event.preventDefault();
-  const instance = basicLightbox.create(
-    `<img src="${event.target.dataset.source}">`,
-    {
-      onShow: () => document.addEventListener("keydown", handleKeyPress),
-      onClose: () => document.removeEventListener("keydown", handleKeyPress),
-    }
-  );
-  instance.show();
-
-  function handleKeyPress(event) {
-    if (event.code === "Escape") {
-      instance.close();
-    }
-  }
-}
+const parseData = JSON.parse(savedData);
+console.log("parseData:", parseData);
